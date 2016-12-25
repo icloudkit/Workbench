4ddffcb22a759990119991845146cb59a2de984f
icloudkit <hongquanli@qq.com>
2016-12-26 00:36:10 +0800

    //

--- app/src/main/java/cn/com/teamlink/workbench/MainActivity.java
+++ app/src/main/java/cn/com/teamlink/workbench/MainActivity.java
@@ -6,11 +6,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +25,29 @@ public class MainActivity extends AppCompatActivity {
         // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         // view.setKeepScreenOn(true)
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        // 用toolbar替换actionbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // app logo
+        // toolbar.setLogo(R.mipmap.ic_launcher);
+        // title
+        toolbar.setTitle(R.string.app_name);
+        // sub title
+        // toolbar.setSubtitle(ToolBar subtitle");
+        // 以上3个属性必须在setSupportActionBar(toolbar)之前调用
+        setSupportActionBar(toolbar);
+        // 设置导航Icon，必须在setSupportActionBar(toolbar)之后设置
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_more);
+        // 添加菜单点击事件
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(toolbar, "Click setNavigationIcon", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+        toolbar.setOnMenuItemClickListener(onMenuItemClickListener);
 
         /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -44,4 +69,34 @@ public class MainActivity extends AppCompatActivity {
         }
         super.onResume();
     }
+
+    /**
+     * 该方法是用来加载菜单布局的
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //加载菜单文件
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    private Toolbar.OnMenuItemClickListener onMenuItemClickListener=new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()){
+//                case R.id.action_search://因为使用android.support.v7.widget.SearchView类，可以在onCreateOptionsMenu(Menu menu)中直接设置监听事件
+//                    Snackbar.make(toolbar,"Click Search",Snackbar.LENGTH_SHORT).show();
+//                    break;
+                case R.id.action_share:
+                    Snackbar.make(toolbar,"Click Share",Snackbar.LENGTH_SHORT).show();
+                    break;
+                case R.id.action_more:
+                    Snackbar.make(toolbar,"Click More",Snackbar.LENGTH_SHORT).show();
+                    break;
+            }
+            return true;
+        }
+    };
 }
