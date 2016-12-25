5f3daaccd9fd12f1802cf9164a1c044c892049f2
icloudkit <hongquanli@qq.com>
2016-12-25 23:48:19 +0800

    //

--- app/src/main/java/cn/com/teamlink/workbench/TestActivity.java
+++ app/src/main/java/cn/com/teamlink/workbench/TestActivity.java
@@ -1,26 +1,18 @@
 package cn.com.teamlink.workbench;
 
-import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 隐藏标题
-        // requestWindowFeature(Window.FEATURE_NO_TITLE);
-        // 设置全屏
-        // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
-        // view.setKeepScreenOn(true)
+        setContentView(R.layout.activity_test);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -34,12 +26,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onResume() {
-        // 设置为横屏
-        if (getRequestedOrientation() != ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        }
-        super.onResume();
-    }
 }
