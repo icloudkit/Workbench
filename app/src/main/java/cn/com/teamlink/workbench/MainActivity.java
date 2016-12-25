279b6a0348e7302c274db13ba2486cdf304e6162
icloudkit <hongquanli@qq.com>
2016-12-25 23:12:42 +0800

    Initial commit

---
+++ app/src/main/java/cn/com/teamlink/workbench/MainActivity.java
@@ -0,0 +1,32 @@
+package cn.com.teamlink.workbench;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 隐藏标题
+        // requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 设置全屏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        // view.setKeepScreenOn(true)
+    }
+
+    /*
+    @Override
+    protected void onResume() {
+        // 设置为横屏
+        if (getRequestedOrientation() != ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+        super.onResume();
+    }
+    */
+}
